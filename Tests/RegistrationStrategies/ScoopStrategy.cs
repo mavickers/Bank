e0bd2ecccd1466301b4ec66374ad97838f345067
Michael A. Vickers <mavickers@lightpathllc.com>
2025-01-06 15:59:10 -0500

    Add simple test for scoop strategy

---
+++ Tests/RegistrationStrategies/ScoopStrategy.cs
@@ -0,0 +1,35 @@
+﻿using LightPath.Bank;
+using System.Reflection;
+
+namespace Tests.RegistrationStrategies
+{
+    public class ScoopStrategy
+    {
+        [Fact]
+        public void Basic()
+        {
+            var @namespace = "StrategyTestAssets.DotNetMvcStrategy.Views";
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceNames = assembly.GetManifestResourceNames();
+            var filteredResourceNames = resourceNames.Where(name => name.StartsWith($"{assembly.GetName().Name}.{@namespace}")).ToList();
+            var assets = new LightPath.Bank.RegistrationStrategies.ScoopStrategy(assembly, @namespace);
+
+            BankAssets.Register(assets);
+
+            Assert.Equal(3, assets.All.Count);
+            Assert.Equal(3, filteredResourceNames.Count);
+
+            foreach (var asset in assets.All)
+            {
+                Assert.NotNull(BankAssets.All[asset.Key]);
+
+                using var stream = assembly.GetManifestResourceStream(asset.Value.ResourceKey);
+                using var reader = stream == null ? null : new StreamReader(stream);
+                var resourceContents = reader?.ReadToEnd();
+                var assetContents = BankAssets.All[asset.Key].Contents.AsString();
+
+                Assert.Equal(resourceContents, assetContents);
+            }
+        }
+    }
+}
