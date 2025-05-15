a3b002a07b23769b641997043558f8c0e7a6803b
Michael A. Vickers <mavickers@lightpathllc.com>
2025-05-15 11:01:22 -0400

    Update publish batch file

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if "%1" == "-local" nuget add Bank\bin\Release\LightPath.Bank.%2.nupkg -source "d:\Projects\LPS\Local Packages" && goto:eof
+if "%1" == "-local" nuget add Bank\bin\Release\LightPath.Bank.%2.nupkg -source "d:\Local Packages" && goto:eof
 if "%1" == "-nuget" nuget push Bank\bin\Release\LightPath.Bank.%2.nupkg -source https://api.nuget.org/v3/index.json && goto:eof
 
 :usage
